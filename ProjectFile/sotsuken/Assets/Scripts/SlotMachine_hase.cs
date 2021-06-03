475a6b813facb4c2880d4d53ec9d3267799ce1b0
hase_koki <ocs.hase.koki@gmail.com>
2021-06-03 15:04:49 +0900

    feat:リール回転

--- ProjectFile/sotsuken/Assets/Scripts/SlotMachine_hase.cs
+++ ProjectFile/sotsuken/Assets/Scripts/SlotMachine_hase.cs
@@ -6,63 +6,10 @@ using UnityEngine.UI;
 
 public class SlotMachine_hase : MonoBehaviour
 {
-    //エディタ上で定義できる
-    [SerializeField]
-    Button btn;         //ボタン
-    [SerializeField]
-    Text text;          //テキスト
-
     int symbolLeft;
     int symbolCenter;
     int symbolRight;
-    Action<int> action;     //戻り値無し
-    Func<int, int> func;    //戻り値あり
 
-    Dictionary<int, Action<int>> aclist;
-    delegate void Test();
-    public void Main()
-    {
-        aclist.Add(1,hoge);
-        aclist.Add(2,moge);
-    }
-    public void hoge(int a)
-    {
-        Debug.Log(111);
-    }
-    public void moge(int a)
-    {
-        Debug.Log("あああ");
-    }
-
-    public void hage(int a)
-    {
-        Debug.Log(111);
-        Debug.Log("あああ");
-    }
-    public void aaa()
-    {
-        /*
-        action = a =>  Debug.Log(a);
-        action += hoge;
-        action += moge;
-        action -= hoge;
-        action(1);
-        */
-        action = hoge;
-        action += moge;
-
-        action(1);
-        hage(1);
-
-    }
-    public void fff(Action<int> ac)
-    {
-        ac(1);
-    }
-    public void ggg(Action<int> ac)
-    {
-        ac(2);
-    }
 
     public static Dictionary<Role, (int l, int c, int r)> dic = new Dictionary<Role, (int l, int c, int r)>()
     {
@@ -70,8 +17,13 @@ public class SlotMachine_hase : MonoBehaviour
         {Role.STRONGCHERRY,(1,1,1)},
         {Role.CHERRY,(1,1,0)  },
         {Role.WEAKCHERRY,(1, 0,0) },
-        {Role.REGBONUS,(2, 2, 2) },
-        {Role.WATERMELON,(3, 3, 3) },
+        {Role.WATERMELON,(2, 2, 2) },
+        {Role.BELL,(3, 3, 3) },
+        {Role.REPLAY,(4, 4, 4) },
+        {Role.QUESTION,(5, 5, 5) },
+        {Role.REGBONUS,(7, 7, 6) },
+        {Role.BIGBONUS,(7, 7, 7) },
+        {Role.FREEZE,(7, 7, 7) },
     };
 
     void Start()
@@ -81,7 +33,7 @@ public class SlotMachine_hase : MonoBehaviour
     void PushSlot()
     {
         //役生成
-        int rand = UnityEngine.Random.Range(0, 5);
+        int rand = UnityEngine.Random.Range(0, 8);
         
         Debug.Log("小役:" + ((Role)rand).ToString() + " ID:" + rand);
         Debug.Log(dic[(Role)rand]);
@@ -98,19 +50,25 @@ public enum Symbol
 {
     NONE = 0,
     CHERRY = 1,
-    BAR = 2,
-    WATERMELON = 3,
-    SEVEN = 4,
-    BELL = 5,
-    REPLAY = 6,
-    QUESTION = 7
+    WATERMELON = 2,
+    BELL = 3,
+    REPLAY = 4,
+    QUESTION = 5,
+    BAR = 6,
+    SEVEN = 7,
+    FREEZE = 8
 }
 public enum Role
 {
-    NONE = 0,
-    WEAKCHERRY = 1,
-    CHERRY = 2,
-    STRONGCHERRY = 3,
-    REGBONUS = 4,
-    WATERMELON = 5,
+    FREEZE = 0,
+    BIGBONUS = 1,
+    REGBONUS = 2,
+    BELL = 3,
+    STRONGCHERRY = 4,
+    CHERRY = 5,
+    WEAKCHERRY = 6,
+    WATERMELON = 7,
+    QUESTION = 8,
+    REPLAY = 9,
+    NONE = 10,
 }
---
+++ ProjectFile/sotsuken/Assets/Scripts/TateScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TateScroll : MonoBehaviour
+{
+    //スクロールスピード
+    [SerializeField] float speed = 1;
+    void Update()
+    {
+        //下方向にスクロール
+        transform.localPosition += Vector3.up*speed;
+        Debug.Log(transform.localPosition);
+        Debug.Log(transform.position);
+        //Yが-11まで来れば、21.33まで移動する
+        if (transform.localPosition.y >= 250f)
+        {
+            transform.localPosition = new Vector2(0, -450f);
+        }
+    }
+}
