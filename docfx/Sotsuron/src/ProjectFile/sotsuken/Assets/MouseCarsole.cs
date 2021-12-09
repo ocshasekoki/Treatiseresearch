5c76aa18788af35ad7c84f928eff1496317b3306
LAB-OCS\184031 <ocs.hase.koki@gmail.com>
2021-12-09 13:56:11 +0900

    feat:演出

---
+++ docfx/Sotsuron/src/ProjectFile/sotsuken/Assets/MouseCarsole.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCarsole : MonoBehaviour
+{
+    [SerializeField] GameObject effect;
+    private Vector3 position;
+    private GameObject obj;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            position = Input.mousePosition; 
+            obj = Instantiate(effect, Camera.main.ScreenToWorldPoint(position), Quaternion.identity);
+            obj.transform.position = new Vector3 (obj.transform.position.x, obj.transform.position.y, 0f);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(obj);
+        }
+        try
+        {
+            Debug.Log(obj.transform.position);
+            position = Input.mousePosition;
+            obj.transform.position = Camera.main.ScreenToWorldPoint(position);
+            obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, 0f);
+        }
+        catch
+        {
+
+        }
+    }
+}
